c95458d01bde93c4af255ad95612f4256ee87098
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-03-07 11:30:23 +0530

    Include api endpoint constant
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

---
+++ src/constants/ApiConstant.js
@@ -0,0 +1,8 @@
+export const BACKEND_API = {
+  BACKEND_BASE_URL: import.meta.env.VITE_BACKEND_URL,
+  LOGIN: "/api/login",
+  REGISTER: "/api/register",
+  USER: "/api/user",
+  SHOP: "/api/shop",
+  ITEM: "/api/item",
+};
